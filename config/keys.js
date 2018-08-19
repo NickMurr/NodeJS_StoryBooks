934ff8987da7c161b0155ceac772bc82ebccca16
Mykyta Muraviov <32221055+NickMurr@users.noreply.github.com>
2018-08-19 16:36:04 +0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI: 'mongodb://nick:nikitadiana30W_@ds125502.mlab.com:25502/storybooks-d',
-  googleClientID: '626134850545-unjvgojolul81cuf4s5npl1vgehs78f6.apps.googleusercontent.com',
-  googleClientSecret: 'a172xoh9d-3XfRWKO0KTxkUp',
+  mongoURI: 'DATA',
+  googleClientID: 'DATA',
+  googleClientSecret: 'DATA',
 };
